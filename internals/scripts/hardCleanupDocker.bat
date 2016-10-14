8e6394c1c55238bfb525c54ab78561e131b9dd35
Christopher Gerlier <christopher.gerlier@crfhealth.com>
2016-10-14 12:24:36 +0300

    minor changes

--- internals/scripts/hardCleanupDocker.bat
+++ internals/scripts/hardCleanupDocker.bat
@@ -1,9 +1,12 @@
 docker stop sso-gateway
 docker rm sso-gateway
 docker rmi sso-gateway
-docker stop sso-gateway-pg
-docker rm sso-gateway-pg
-docker rmi sso-gateway-pg
+docker stop sso-gateway-postgres
+docker rm sso-gateway-postgres
+docker rmi sso-gateway-postgres
+docker stop sso-gateway-redis
+docker rm sso-gateway-redis
+docker rmi sso-gateway-redis
 rmdir /Q /s %~dp0..\..\node_modules
 rmdir /Q /s %~dp0..\..\build
 del /Q /s %~dp0..\..\*.gzip
